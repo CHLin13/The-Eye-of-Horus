fd87676f766f599c1bfbf98c523d63663b81eaac
CHLin13 <lch132502@gmail.com>
2022-04-13 17:13:51 +0800

    view- install exphbs, build main and login page

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const exphbs = require('express-handlebars');
 const port = process.env.PORT;
 
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send('hi')
-})
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
