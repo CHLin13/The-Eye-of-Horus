60eeb525689e048d3c48d0839ec6d6181172a1e4
CHLin13 <lch132502@gmail.com>
2022-04-14 10:17:06 +0800

    view- build dashboard related page

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const app = express();
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
-
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
