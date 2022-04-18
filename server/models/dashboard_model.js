0eb5315f075630f6e963c6f6a8412c6ac8339380
CHLin13 <lch132502@gmail.com>
2022-04-18 19:19:28 +0800

    refector- add getChartDetail, getCharts, previewChart module

--- server/models/dashboard_model.js
+++ server/models/dashboard_model.js
@@ -91,7 +91,62 @@ const postChart = async (req) => {
   }
 };
 
+const getChartDetail = async (dashboardId, chartId) => {
+  const chartQuery = 'SELECT * FROM chart WHERE id = ?';
+  const [chart] = await pool.query(chartQuery, chartId);
+  const layout = JSON.parse(chart[0].layout);
+  const data = {
+    dashboardId: dashboardId,
+    chartId: chartId,
+    title: layout.title,
+    titleFontSize: layout.titlefont.size,
+    timeRange: chart[0].time_range,
+    source: chart[0].database + '/' + chart[0].measurement,
+    style: chart[0].chart_type,
+    interval: chart[0].interval,
+    interval_unit: chart[0].interval_unit,
+    select: chart[0].select,
+    xAxisTitle: layout.xaxis.title,
+    xAxisFontSize: layout.xaxis.titlefont.size,
+    xAxisTickFontSize: layout.xaxis.tickfont.size,
+    yAxisTitle: layout.yaxis.title,
+    yAxisFontSize: layout.yaxis.titlefont.size,
+    yAxisTickFontSize: layout.yaxis.tickfont.size,
+  };
+  return data;
+};
+
+const getCharts = async (dashboardId) => {
+  const chartQuery =
+    'select * from dashboard inner join chart on chart.dashboard_id = dashboard.id where chart.dashboard_id = ?';
+  const [chart] = await pool.query(chartQuery, dashboardId);
+  for (let i = 0; i < chart.length; i++) {
+    chart[i].dashboardId = dashboardId;
+  }
+  return chart;
+};
+
+const previewChart = async (req) => {
+    const { timeRange, source, interval, interval_unit, select } = req.body;
+    const database = source.split('/')[0];
+    const measurement = source.split('/')[1];
+    const influxdb = new Influxdb.InfluxDB(process.env.URL + database);
+
+    const intervalN = interval * units.timeUnits[interval_unit];
+    const rangeIntoSec =
+      timeRange.split('-')[0] * units.timeUnits[timeRange.split('-')[1]];
+    const limit = Math.floor(rangeIntoSec / intervalN);
+
+    const system = await influxdb.query(
+      `select ${select}(*) from ${measurement} GROUP BY type_instance, time(${interval}${interval_unit}) order by DESC limit ${limit}`
+    );
+  return system.groupRows;
+};
+
 module.exports = {
   getSource,
   postChart,
+  getChartDetail,
+  getCharts,
+  previewChart,
 };
--- server/routes/dashboard_route.js
+++ server/routes/dashboard_route.js
@@ -3,7 +3,13 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../../configs/mysqlConnect');
 const units = require('../../utils/units');
-const { getSource, postChart } = require('../models/dashboard_model');
+const {
+  getSource,
+  postChart,
+  getChartDetail,
+  getCharts,
+  previewChart,
+} = require('../models/dashboard_model');
 const Influxdb = require('influx');
 
 router.get('/', async (req, res) => {
@@ -11,21 +17,8 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/chart/preview', async (req, res) => {
-  const { timeRange, source, interval, interval_unit, select } = req.body;
-  const database = source.split('/')[0];
-  const measurement = source.split('/')[1];
-  const influxdb = new Influxdb.InfluxDB(process.env.URL + database);
-
-  const intervalN = interval * units.timeUnits[interval_unit];
-  const rangeIntoSec =
-    timeRange.split('-')[0] * units.timeUnits[timeRange.split('-')[1]];
-  const limit = Math.floor(rangeIntoSec / intervalN);
-
-  const system = await influxdb.query(
-    `select ${select}(*) from ${measurement} GROUP BY type_instance, time(${interval}${interval_unit}) order by DESC limit ${limit}`
-  );
-  const data = system.groupRows;
-  return res.json({ data: data, select: select });
+  const data = await previewChart(req);
+  return res.json({ data: data, select: req.body.select });
 });
 
 router.get('/:dashboardId/create', async (req, res) => {
@@ -41,42 +34,14 @@ router.post('/:dashboardId/create', async (req, res) => {
 
 router.get('/:dashboardId', async (req, res) => {
   const dashboardId = req.params.dashboardId;
-  const [chart] = await pool.query(
-    `select * from dashboard inner join chart on chart.dashboard_id = dashboard.id where chart.dashboard_id = ?`,
-    [dashboardId]
-  );
-
-  for (let i = 0; i < chart.length; i++) {
-    chart[i].dashboardId = dashboardId;
-  }
-
+  const chart = await getCharts(dashboardId);
   return res.render('dashboard_detail', { chart });
 });
 
 router.get('/:dashboardId/chart/:chartId', async (req, res) => {
   const { dashboardId, chartId } = req.params;
+  const data = await getChartDetail(dashboardId, chartId);
   const source = await getSource();
-  const [chart] = await pool.query(`select * from chart where id = ?`, [chartId]);
-  const layout = JSON.parse(chart[0].layout);
-  const data = {
-    dashboardId: dashboardId,
-    chartId: chartId,
-    title: layout.title,
-    titleFontSize: layout.titlefont.size,
-    timeRange: chart[0].time_range,
-    source: chart[0].database + '/' + chart[0].measurement,
-    style: chart[0].chart_type,
-    interval: chart[0].interval,
-    interval_unit: chart[0].interval_unit,
-    select: chart[0].select,
-    xAxisTitle: layout.xaxis.title,
-    xAxisFontSize: layout.xaxis.titlefont.size,
-    xAxisTickFontSize: layout.xaxis.tickfont.size,
-    yAxisTitle: layout.yaxis.title,
-    yAxisFontSize: layout.yaxis.titlefont.size,
-    yAxisTickFontSize: layout.yaxis.tickfont.size,
-  };
-
   return res.render('create', { data, source });
 });
 
