bf0ae3cfe14980b83f304557f88d1a312459cae0
CHLin13 <lch132502@gmail.com>
2022-04-20 21:02:06 +0800

    view- build alert_create page

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
-const methodOverride = require('method-override');  
-const handlebarsHelpers = require('./utils/handlebars-helpers')
+const methodOverride = require('method-override');
+const handlebarsHelpers = require('./utils/handlebars-helpers');
 const port = process.env.PORT;
 
 const app = express();
@@ -16,7 +16,7 @@ app.engine(
   })
 );
 app.set('view engine', 'hbs');
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
