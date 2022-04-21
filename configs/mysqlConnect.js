5b618f053d0017bfdc59ddc645b17490999fb82c
CHLin13 <lch132502@gmail.com>
2022-04-21 18:54:22 +0800

    refector- rename env parameters

--- configs/mysqlConnect.js
+++ configs/mysqlConnect.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
-  user: process.env.Mysql_user,
-  password: process.env.Mysql_password,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: 'eye',
 });
 
