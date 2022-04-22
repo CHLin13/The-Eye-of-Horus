7bb68b7b06ad140ebcbbb49e46d6ce5031903d23
CHLin13 <lch132502@gmail.com>
2022-04-22 11:37:02 +0800

    view- add css

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.engine(
   })
 );
 app.set('view engine', 'hbs');
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
