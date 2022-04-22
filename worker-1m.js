2b54fd6bfe66935bb7f48ac0ec8998ba16259e27
CHLin13 <lch132502@gmail.com>
2022-04-22 18:59:35 +0800

    feat- add notify feature

--- worker-1m.js
+++ worker-1m.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const redis = require('./configs/redisConnect');
 const Influxdb = require('influx');
+const axios = require('axios');
+const mailgun = require('mailgun-js');
+const DOMAIN = 'baboo.shop';
 const units = require('./utils/units');
+
+const mg = mailgun({
+  apiKey: process.env.MGAPIKEY,
+  domain: DOMAIN,
+});
+const { WebhookClient } = require('discord.js');
 
 const work = (async function () {
   const response = await redis.hGetAll('1m');
@@ -24,7 +33,6 @@ const work = (async function () {
     const system = await influxdb.query(
       `select ${result[i].select}(*) from ${measurement} WHERE type_instance = '${result[i].type}' GROUP BY time(${result[i].eval_every_input}) order by DESC limit ${limit}`
     );
-
     const select = result[i].select + '_value';
     let count = 0;
     for (let j = 0; j < limit; j++) {
@@ -53,7 +61,38 @@ const work = (async function () {
       }
 
       if (count === 5) {
-      console.log(result[i].message);
+        const detail = JSON.parse(result[i].receiver_detail);
+        const errorMessage = result[i].message;
+        switch (result[i].receiver_type) {
+          case 'Email':
+            const data = {
+              from: 'The Eye of Horus <TheEyeofHorus@baboo.shop>',
+              to: detail[0],
+              subject: 'Alert from The Eye of Horus',
+              html: `<h1>${errorMessage}</h1>`,
+            };
+            mg.messages().send(data);
+            break;
+          case 'Slack':
+            axios.post(detail[0], {
+              text: errorMessage,
+              icon_url: 'https://i.imgur.com/euLn4Te.png',
+              username: ' The-Eye-of-Horus',
+            });
+            break;
+          case 'Discord':
+            const webhookClient = new WebhookClient({
+              id: detail[0],
+              token: detail[1],
+            });
+
+            webhookClient.send({
+              content: errorMessage,
+              username: 'The-Eye-of-Horus',
+              avatarURL: 'https://i.imgur.com/euLn4Te.png',
+            });
+            break;
+        }
       }
     }
   }
