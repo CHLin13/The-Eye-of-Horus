452955248e7df588d0bdc88153907d3129ae7b2c
CHLin13 <lch132502@gmail.com>
2022-05-01 10:09:14 +0800

    feat- add ifCond helper

--- utils/handlebars-helpers.js
+++ utils/handlebars-helpers.js
@@ -2,4 +2,36 @@ module.exports = {
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this);
   },
+  ifNotEq: function (a, b, options) {
+    return a !== b ? options.fn(this) : options.inverse(this);
+  },
+  // ifCond: function (v1, operator, v2, options) {
+  //   switch (operator) {
+  //     case '==':
+  //       return v1 == v2 ? options.fn(this) : options.inverse(this);
+  //     case '===':
+  //       return v1 === v2 ? options.fn(this) : options.inverse(this);
+  //     case '!=':
+  //       return v1 != v2 ? options.fn(this) : options.inverse(this);
+  //     case '!==':
+  //       return v1 !== v2 ? options.fn(this) : options.inverse(this);
+  //     case '<':
+  //       return v1 < v2 ? options.fn(this) : options.inverse(this);
+  //     case '<=':
+  //       return v1 <= v2 ? options.fn(this) : options.inverse(this);
+  //     case '>':
+  //       return v1 > v2 ? options.fn(this) : options.inverse(this);
+  //     case '>=':
+  //       return v1 >= v2 ? options.fn(this) : options.inverse(this);
+  //     case '&&':
+  //       return v1 && v2 ? options.fn(this) : options.inverse(this);
+  //     case '||':
+  //       return v1 || v2 ? options.fn(this) : options.inverse(this);
+  //     default:
+  //       return options.inverse(this);
+  //   }
+  // },
+  log: function (something) {
+    console.log(something);
+  },
 };
