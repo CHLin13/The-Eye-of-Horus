188f67fb8be7753a68c895810622b06a459ff271
CHLin13 <lch132502@gmail.com>
2022-05-02 11:37:20 +0800

    fix- adjust session maxage

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ try {
       cookie: {
         secure: false, // if true only transmit cookie over https
         httpOnly: false, // if true prevent client side JS from reading the cookie
-        maxAge: 1000 * 60 * 30, // session max age in miliseconds
+        maxAge: 1000 * 60 * 60 * 10, // session max age in miliseconds
       },
     })
   );
