1ee68ff122adb7e8cca42b97c7e60e9597202b89
CHLin13 <lch132502@gmail.com>
2022-05-05 17:44:37 +0800

    feat- alert validation

--- server/routes/alert_route.js
+++ server/routes/alert_route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 
 const alertController = require('../controllers/alert_controller');
-const dashboardController = require('../controllers/dashboard_controller')
+const dashboardController = require('../controllers/dashboard_controller');
 const { authenticated, authenticatedSuper } = require('../../utils/auth');
 
 router.get('/', authenticated, alertController.getAlertList);
@@ -12,11 +13,47 @@ router.post(
   '/chart/preview',
   authenticated,
   authenticatedSuper,
+  [
+    body('timeRange').not().isEmpty(),
+    body('source').not().isEmpty(),
+    body('interval').not().isEmpty(),
+    body('interval_unit').not().isEmpty(),
+    body('select').not().isEmpty(),
+  ],
   dashboardController.chartPreview
 );
-router.post('/', authenticatedSuper, alertController.postAlert);
+
+router.post(
+  '/',
+  authenticatedSuper,
+  [
+    body('name').not().isEmpty(),
+    body('source').not().isEmpty(),
+    body('select').not().isEmpty(),
+    body('condition').not().isEmpty(),
+    body('eval_every_input').not().isEmpty(),
+    body('eval_for_input').not().isEmpty(),
+    body('receiver_id').not().isEmpty(),
+    body('message').not().isEmpty(),
+  ],
+  alertController.postAlert
+);
 router.get('/:alertId', authenticatedSuper, alertController.getAlert);
-router.put('/:alertId', authenticatedSuper, alertController.postAlert);
+router.put(
+  '/:alertId',
+  authenticatedSuper,
+  [
+    body('name').not().isEmpty(),
+    body('source').not().isEmpty(),
+    body('select').not().isEmpty(),
+    body('condition').not().isEmpty(),
+    body('eval_every_input').not().isEmpty(),
+    body('eval_for_input').not().isEmpty(),
+    body('receiver_id').not().isEmpty(),
+    body('message').not().isEmpty(),
+  ],
+  alertController.postAlert
+);
 router.delete('/:alertId', authenticatedSuper, alertController.deleteAlert);
 
 module.exports = router;
