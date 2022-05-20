b8bf1f936107a433fe4012055ceef0e8a178473f
CHLin13 <lch132502@gmail.com>
2022-05-20 18:42:13 +0800

    test- create fake_data file

---
+++ test/fake_data.js
@@ -0,0 +1,13 @@
+const users = [
+  {
+    name: 'super',
+    email: 'super@example.com',
+    password: 'test1password',
+    superuser: 0,
+    status: 1,
+  },
+];
+
+module.exports = {
+  users,
+};
